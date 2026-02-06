5fed4373710dc3b282aa2c66a2ab0d1f3bca7c33
Pravin Banjade <pravinbanjade9@gmail.com>
2026-02-06 18:52:21 +0545

    add website switch btn

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -6,7 +6,7 @@ const FloatingActionButton = () => {
   const [showTooltip, setShowTooltip] = useState(false);
 
   const redirectToTechyWebsite = () => {
-    window.open('https://pravinb.com.np', '_blank');
+    window.open('https://pravinb.com.np');
   };
 
   return (
